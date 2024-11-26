f2c306bd78570ff4b666f6e531d35bfcd4899484
TomBoutin <tomboutin.tom@gmail.com>
2024-11-26 14:42:15 +0100

    switch codespace

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { HomeIcon, UserGroupIcon, CalendarDateRangeIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { HomeIcon, UserGroupIcon, CalendarDateRangeIcon } from '@heroicons/react/24/outline';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
