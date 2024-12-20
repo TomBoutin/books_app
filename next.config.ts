fc8b7978fade17867accfe1b6e5142d76ba8bc3d
TomBoutin <tomboutin.tom@gmail.com>
2024-12-20 23:21:39 +0100

    context change

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: ["fictional-goggles-979x44xx49p537gwr-3000.github.dev", "localhost:3000"]
+      allowedOrigins: ["fictional-goggles-979x44xx49p537gwr-3000.github.dev", "localhost:3000", "193.168.145.159:3000"]
     }
   }
   
